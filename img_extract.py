ab47cde7eb036ab406dd3cea4ee8b01ce7fe1dd5
IT Stagiaire5 <itstg5@dpworld.sn>
2021-09-20 12:21:43 +0000

    fixing problems with script

--- img_extract.py
+++ img_extract.py
@@ -5,7 +5,7 @@ import os
 
 
 data=pd.read_csv('training.csv')
-
+data.dropna(inplace=True)
 def getimage(each):
     img = Image.new( 'RGB', (96,96), "black") 
     pixels = img.load() # create the pixel map
@@ -17,14 +17,14 @@ def getimage(each):
     return img
 
 if __name__=='__main__':
-    data=pd.read_csv('training.csv')
     try:
         os.mkdir(os.getcwd()+'/imgs')
     except:
-        print ('X')
+        print('X')
 
     print('there are '+str(len(data.Image))+' images in total\nstart extracting......')
     for i in range(1,len(data.Image)+1):
-        getimage(data.Image[i-1]).save(os.getcwd()+'/imgs/'+str(i)+'.jpg')
+        row = data.iloc[i-1]
+        getimage(row['Image']).save(os.getcwd()+'/imgs/'+str(i)+'.jpg')
     
     
